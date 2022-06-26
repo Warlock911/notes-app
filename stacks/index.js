8fc45630e6593d4901daa1066329e09b0484606d
Warlock911 <shehanfernando1946@gmail.com>
2022-06-26 21:36:37 +0800

    adding the frontend

--- stacks/index.js
+++ stacks/index.js
@@ -1,6 +1,7 @@
 import { StorageStack } from "./StorageStack";
 import { ApiStack } from "./ApiStack";
 import { AuthStack } from "./AuthStack";
+import { FrontendStack } from "./FrontendStack";
 
 export default function main(app) {
   app.setDefaultFunctionProps({
@@ -11,5 +12,5 @@ export default function main(app) {
     },
   });
   //all the imports are chained below
-  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack).stack(FrontendStack);
 }
